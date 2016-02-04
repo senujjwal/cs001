c845e6b0cd9105cac7a9dcc558131c7294d3022d
Ujjwal Sen <senujjwal@outlook.com>
2016-02-04 22:50:50 +0530

    Adding simple console I/O

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 void main()
 {
-	cout << "Hello World!";
+	int num1, num2;
+	cout << "Enter two numbers separated by a space: ";
+	cin >> num1 >> num2;
+	cout << "The sum is: " << (num1 + num2);
 }
